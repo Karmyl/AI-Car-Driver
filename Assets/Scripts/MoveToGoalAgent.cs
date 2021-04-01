f6eb14dd33bd454a9956527c3a917d58d882b5fc
Karri Myller <1905311@edu.karelia.fi>
2021-04-01 11:35:16 +0300

    ML-Agents: Start

---
+++ Assets/Scripts/MoveToGoalAgent.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class MoveToGoalAgent : Agent
+{
+    [SerializeField] private Transform targetTranform;
+    [SerializeField] private float moveSpeed = 1f;
+
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(0f, -0.5f, 1f);
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTranform.localPosition);
+    }
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+
+        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collide happened!");
+        if (other.TryGetComponent<Goal>(out Goal goal))
+        {
+            SetReward(1f);
+            EndEpisode();
+        }
+        if (other.TryGetComponent<Wall>(out Wall wall))
+        {
+            SetReward(-1f);
+            EndEpisode();
+        }
+    }
+
+}
+
